7272d8df2e530c6e096a15c1518dd4cc18ea13b8
Jaspher Jan Pasol <jpasol@sbitc.ph>
2018-12-12 16:30:12 +0800

    Removed Interface ICraneLogsReport

--- Project1/IReports.vb
+++ Project1/IReports.vb
@@ -83,30 +83,5 @@
 
     End Interface
 
-    Public Interface ICraneLogsReport
-        ReadOnly Property Vessel As Vessel
-        ReadOnly Property Crane() As System.Collections.Generic.List(Of Crane)
-        ReadOnly Property TotalMoves As Double
-        ReadOnly Property FirstMove As Date
-        ReadOnly Property LastMove As Date
-        ReadOnly Property CraneDensity As Double
-        ReadOnly Property TotalBerthHours As Double
-        ReadOnly Property NetBerthHours As Double
-        ReadOnly Property GrossBerthProdRate As Double
-        ReadOnly Property NetBerthProdRate As Double
-        ReadOnly Property GrossVesselWorkingTime As Double
-        ReadOnly Property NetVesselWorkingTime As Double
-        ReadOnly Property GrossVesselProdRate As Double
-        ReadOnly Property NetVesselProdRate As Double
-        ReadOnly Property TotalGrossWorkingHours As Double
-        ReadOnly Property TotalNetWorkingHours As Double
-        ReadOnly Property GrossCraneProductivity As Double
-        ReadOnly Property NetCraneProductivity As Double
 
-        Property LastPort
-        Property NextPort
-
-        Sub IntializeCrane(GantryName As String)
-
-    End Interface
 End Module
